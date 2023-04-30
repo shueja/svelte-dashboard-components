6f4016e10b8b6183292a03ac82499b838acc2f2e
shueja-personal <j.a.shue@outlook.com>
2023-04-29 21:25:34 -0700

    Made grid layout units square

--- src/web-components/layout/GridLayout.svelte
+++ src/web-components/layout/GridLayout.svelte
@@ -1,12 +1,27 @@
 <script>
     export let columns;
     export let rows;
-    export let unitHeight = undefined;
+    export let square=true;
+    let gridWidth;
+    let gridHeight;
+    let unitHeight = undefined;
+    let unitWidth = undefined;
+    let columnsCSS;
+    let rowsCSS;
+    $: if (!square) {
+        columnsCSS = `repeat(${columns}, calc(${gridWidth}px / ${columns}))`
+        rowsCSS = `repeat(${rows}, calc(${gridHeight}px / ${rows}))`
+    } else {
+        columnsCSS = `repeat(${columns}, calc(${gridWidth}px / ${columns}))`
+        rowsCSS = `repeat(${rows}, calc(${gridWidth}px / ${columns}))`
+    }
 </script>
-<div style="
+<div bind:clientHeight={gridHeight} bind:clientWidth={gridWidth}
+    style="
     display:grid;
     width: 100%;
     height: 100%;
-    grid-template-columns:repeat({columns}, {unitHeight==undefined ? `calc(100%/${columns})` : unitHeight}); grid-template-rows:repeat({rows}, calc(100%/{rows}))">
+    grid-template-columns:{columnsCSS};
+    grid-template-rows:{rowsCSS};">
 <slot/>
 </div>
