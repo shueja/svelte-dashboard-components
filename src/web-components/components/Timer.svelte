e793852ec2e7c68b83f02406b9ce25b22e45e1c7
shueja-personal <j.a.shue@outlook.com>
2023-05-08 22:58:01 -0700

    Removed 100ths of a second on Timer

--- src/web-components/components/Timer.svelte
+++ src/web-components/components/Timer.svelte
@@ -32,9 +32,9 @@
     bind:clientWidth={w} bind:clientHeight={h}>
     <div style="height:10%; width:100%; font-size: calc(0.2 * {h}px)">{label}</div>
     {#if hms}
-    {value === -1 ? "--": `${Math.floor(value / 60).toFixed(0)}:${(value % 60).toFixed(2).toString().padStart(5, '0')}`}
+    {value === -1 ? "--": `${Math.floor(value / 60).toFixed(0)}:${(value % 60).toFixed(1).toString().padStart(4, '0')}`}
     {:else}
-    {value === -1 ? "--": value.toFixed(2)}
+    {value === -1 ? "--": value.toFixed(1)}
     {/if}
     {#if showBar}
     <div class="bar" style="width:calc(100% * max(0, min({max == 0 ? 0 : value / max}, 1)));"></div>
